fb35f01c753f43457e1a19a920d4f90c11e9a230
jasmeet.singh <jasmeet.singh@semaphoresolutions.ca>
2018-11-24 13:25:24 -0800

    Subscribe and unsubscribe Observable

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,34 @@
 // import * as Rx from "rxjs/Observable";
 import { Observable } from "rxjs"
 
-console.log(Observable)
+var observale = Observable.create((observer: any)=>{
+  try {
+    observer.next("This is Hello")
+    observer.next("This is another Hello")
+    setInterval(()=>{
+      observer.next("I'm good.")
+    }, 2000)
+    // observer.complete("This is complete Hello")
+    observer.next("This is Hello won't be listened")   
+  } catch (error) {
+    observer.error(error)    
+  }
+  
+})
+
+var observer = observale.subscribe(
+                (x:any) => addItem(x),
+                (err:any) => addItem(err),
+                () => addItem("Completed")
+              );
+
+setTimeout(()=>{
+  observer.unsubscribe()
+}, 6001)
+
+function addItem(val:any) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
+}
